1f951204a9805e4715b74c964f06fc47a5f3aef7
vladi <vladislavcekov2003@gmail.com>
2022-03-08 15:15:08 +0200

    SVETNA

---
+++ School-Management/src/main/resources/sql/schema.sql
@@ -0,0 +1,5 @@
+create table if not exists author(
+                                     id SERIAL primary key,
+                                     name varchar (255)
+    ,last_name varchar (255)
+    );
